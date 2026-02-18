2f6524ed22776fd75faf8adbd0099b25bc47c1c2
seajoyer <seajoyer@gmail.com>
2026-02-18 07:44:36 +0300

    First commit

---
+++ devenv.nix
@@ -0,0 +1,57 @@
+{ pkgs, ... }:
+
+{
+  packages = with pkgs; [
+    git
+    pwgen
+    docker-compose 
+  ];
+
+  languages.python = {
+    enable = false;
+    version = "3.14";
+    uv = {
+      enable = false;
+      sync.enable = true;
+    };
+    venv.enable = false;
+  };
+
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_18;
+    port = 5432;
+    listen_addresses = "*";
+    initialDatabases = [
+      { name = "project"; }
+      { name = "redash"; }
+    ];
+    initialScript = ''
+      # ALTER ROLE dmitry WITH PASSWORD 'redashpass';
+    '';
+
+    hbaConf = ''
+      # Local Unix socket connections (passwordless, OS user matching or trust)
+      local   all             all                                     trust
+
+      # IPv4 loopback (localhost)
+      host    all             all             127.0.0.1/32            trust
+
+      # IPv6 loopback
+      host    all             all             ::1/128                 trust
+
+      # All other connections (including from Docker containers) - trust for local dev
+      host    all             all             0.0.0.0/0               trust
+      host    all             all             ::/0                    trust
+    '';
+  };
+
+  dotenv.enable = true;
+
+  # Redis (required by Redash)
+  services.redis = {
+    enable = true;
+    bind = "0.0.0.0";
+    port = 6379;
+  };
+}
